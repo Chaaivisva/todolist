b1b80f4b8882930d5cd320305a0f58e3e242f9c1
Chaai visva <chaaivisva303@gmail.com>
2023-05-09 21:07:59 +0530

    add localstorage support and data store in google spreadsheet

--- todojs.js
+++ todojs.js
@@ -2,9 +2,32 @@ const inputbox = document.getElementById("input-box");
 const listcontainer = document.querySelector("#list-container")
 const dateInput = document.querySelector("#date")
 const filter = document.querySelector("#filter")
+let tempDate;
+let tempTodo;
+
+window.addEventListener("load",(e) => {
+
+    let data =JSON.parse(localStorage.getItem("todos"))
+
+    data?.forEach((el) => {
+
+        var doc = new DOMParser().parseFromString(el, "text/html");
+        doc.body.firstChild.lastChild.addEventListener("dblclick",addDblEvent)
+        doc.body.firstChild.lastChild.addEventListener("click",addClickEvent)
+        listcontainer.appendChild(doc.body.firstChild) 
+    })
+
+})
+
+
 
 let count=0;
-function addTask(){
+async function addTask(){
+    tempTodo = inputbox.value
+    tempDate = dateInput.value
+
+    console.log(dateInput.value)
+
     console.log(filter.value)
     if(inputbox.value === '' ){
         alert("You must write something!");
@@ -21,7 +44,7 @@ function addTask(){
         span1.innerHTML+='<br>'
         let date = document.createElement("div")
         date.id = "date"
-        const formateDate = new Date(dateInput.value)
+        const formateDate =  dateInput.value !== "" ? new Date(dateInput.value) : new Date() 
         date.appendChild(document.createTextNode(formateDate.toLocaleDateString()))
         div2.append(span1)
         div2.appendChild(date)
@@ -33,17 +56,47 @@ function addTask(){
         
         let span = document.createElement("span");
         span.className="task"
+        // span.classList.add("delete")
         span.id=count++;
         span.innerHTML = "\u00d7"
+        span.addEventListener("dblclick",addDblEvent)
+        span.addEventListener("click",addClickEvent)
         li.append(span);
+
+        let seperate = document.createElement("span")
+        seperate.innerHTML = "seperate"
+        seperate.classList.add("hide")
+        listcontainer.append(seperate)
+        saveData()
+
     }
-    changeFilter()
     inputbox.value="";
     dateInput.value=""
+
+
+    let formData = new FormData();
+    const formateDate =  tempDate !== "" ? new Date(tempDate) : new Date() 
+    formData.append('todo', tempTodo);
+    formData.append('date', formateDate.toLocaleDateString());
+
+    const response = await fetch("https://script.google.com/macros/s/AKfycbwMEOyzYzBY2evVvINYxkBOtwNWWlzVMMEy9K705cu-ZygiM-OEIOncKbARtn9BvS8/exec",{
+        method:"post",
+        
+        body : formData,
+      
+        
+    })
+
+    changeFilter()
+    // console.log(response)
+
+
+    
     // saveData();
 }
 
-listcontainer.addEventListener("dblclick", function(e){
+
+function addDblEvent(e){
     
     // if(e.target.tagName === "SPAN"){
         //e.target.nextElementSibling
@@ -57,32 +110,40 @@ listcontainer.addEventListener("dblclick", function(e){
         elements.forEach((el) => {
             locCount = e.target.id
             for(let i = locCount+1;i < elements.length;i++){
-                if(elements){
+                if(elements[i]){
                     elements[i].id = i
                 }
             }
         })
 
-        // saveData()
+        saveData()
         // e.target.id=coun t--;
     // }
-});
+}
 
-listcontainer.addEventListener("click", function(e){
-   let ide=e.target.id
-   let previousSibling = document.querySelectorAll(".task")[ide].previousSibling
 
-   previousSibling.style.textDecoration='line-through';
+// https://docs.google.com/spreadsheets/d/1aAt9IFuXGK7OkjEgplaNIc24GSfwOjJDMF4CPSPtZmY/edit#gid=0 --> Spreadsheet link
+
+
+
+ function addClickEvent(e){
+//    let ide=e.target.id
+//    let previousSibling = document.querySelectorAll(".task")[ide].previousSibling
+
+   e.target.parentElement.style.textDecoration='line-through';
   //  let sel=document.listcontainer
   // listcontainer.style.textDecoration='line-through';
-});
+}
 
 function saveData(){
-    localStorage.setItem("data",JSON.stringify(listcontainer.innerHTML))
+
+    let todos = Array.from(document.querySelectorAll("#list"))
+    let data = todos.map((el) => el.outerHTML)
+    localStorage.setItem("todos",JSON.stringify(data))    
 }
 
 function showTask(){
-    listcontainer.innerHTML = JSON.parse(localStorage.getItem("data"))
+    listcontainer.innerHTML = JSON.parse(localStorage.getItem("todos"))
 }
 
 // showTask();
@@ -102,3 +163,7 @@ function changeFilter(){
         }
 
 }
+
+
+
+
